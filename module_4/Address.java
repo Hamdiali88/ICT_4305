37f7727a8a38f1db95188908d47bd970dd1c5b90
Hamdiali88 <hamdi.ali@du.edu>
2025-10-02 19:42:34 -0600

    Create Address.java

---
+++ module_4/Address.java
@@ -0,0 +1,20 @@
+public class Address {
+    private String streetAddress1;
+    private String streetAddress2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address(String streetAddress1, String streetAddress2, String city, String state, String zipCode) {
+        this.streetAddress1 = streetAddress1;
+        this.streetAddress2 = streetAddress2;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getAddressInfo() {
+        return streetAddress1 + ", " + (streetAddress2 != null ? streetAddress2 + ", " : "")
+                + city + ", " + state + " " + zipCode;
+    }
+}
