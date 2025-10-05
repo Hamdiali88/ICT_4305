c81b13ffde9f54ae21dbd58855eef5b5ca045457
Hamdiali88 <hamdi.ali@du.edu>
2025-10-04 22:06:15 -0600

    Create car_test.java

---
+++ module_4/car_test.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+public class CarTest {
+
+    @Test
+    void testCarSpeed() {
+        Car car = new Car("ABC123", CarType.COMPACT, "C001");
+
+        assertNotNull(car.getPermit());
+        assertEquals(CarType.COMPACT, car.getType());
+        assertEquals("C001", car.getOwner());
+        assertTrue(car.getPermitExpiration().isAfter(LocalDate.now()));
+    }
+}
