a1aaef781e76f04f363ab25718e7a1e4e1b5be34
Hamdiali88 <hamdi.ali@du.edu>
2025-10-09 20:47:05 -0600

    Create Month.java

---
+++ module_5/Month.java
@@ -0,0 +1,17 @@
+public class Month<T> {
+    private T month;
+
+    public Month() { }
+
+    public Month(T month) {
+        this.month = month;
+    }
+
+    public T getMonth() {
+        return month;
+    }
+
+    public void setMonth(T month) {
+        this.month = month;
+    }
+}
